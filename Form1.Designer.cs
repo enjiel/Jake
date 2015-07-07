4610b82cf63529cfa9e773625ff867e1c8731faf
Enjiel <enjielkaldwor@gmail.com>
2015-07-07 15:35:30 -0500

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,13 +31,15 @@ namespace Jake
             this.inputBox = new System.Windows.Forms.TextBox();
             this.outputBox = new System.Windows.Forms.TextBox();
             this.imageBox = new System.Windows.Forms.PictureBox();
+            this.launchBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
             this.SuspendLayout();
             // 
             // inputBox
             // 
-            this.inputBox.Location = new System.Drawing.Point(112, 433);
+            this.inputBox.Location = new System.Drawing.Point(163, 433);
             this.inputBox.Name = "inputBox";
+            this.inputBox.ReadOnly = true;
             this.inputBox.Size = new System.Drawing.Size(401, 20);
             this.inputBox.TabIndex = 0;
             // 
@@ -45,7 +47,7 @@ namespace Jake
             // 
             this.outputBox.CausesValidation = false;
             this.outputBox.Enabled = false;
-            this.outputBox.Location = new System.Drawing.Point(112, 303);
+            this.outputBox.Location = new System.Drawing.Point(163, 303);
             this.outputBox.Multiline = true;
             this.outputBox.Name = "outputBox";
             this.outputBox.Size = new System.Drawing.Size(401, 124);
@@ -53,17 +55,28 @@ namespace Jake
             // 
             // imageBox
             // 
-            this.imageBox.Location = new System.Drawing.Point(112, 24);
+            this.imageBox.Location = new System.Drawing.Point(163, 24);
             this.imageBox.Name = "imageBox";
             this.imageBox.Size = new System.Drawing.Size(401, 273);
             this.imageBox.TabIndex = 2;
             this.imageBox.TabStop = false;
             // 
+            // launchBtn
+            // 
+            this.launchBtn.Location = new System.Drawing.Point(609, 116);
+            this.launchBtn.Name = "launchBtn";
+            this.launchBtn.Size = new System.Drawing.Size(75, 23);
+            this.launchBtn.TabIndex = 3;
+            this.launchBtn.Text = "Launch";
+            this.launchBtn.UseVisualStyleBackColor = true;
+            this.launchBtn.Click += new System.EventHandler(this.launchBtn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(654, 534);
+            this.ClientSize = new System.Drawing.Size(749, 534);
+            this.Controls.Add(this.launchBtn);
             this.Controls.Add(this.imageBox);
             this.Controls.Add(this.outputBox);
             this.Controls.Add(this.inputBox);
@@ -80,6 +93,8 @@ namespace Jake
         private System.Windows.Forms.TextBox inputBox;
         private System.Windows.Forms.TextBox outputBox;
         private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.Button launchBtn;
     }
 }
+
 
