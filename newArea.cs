d6ee5899dd0974ad6b9aa7ed9526cb67b01d0268
Enjiel <enjielkaldwor@gmail.com>
2015-07-07 15:41:27 -0500

    Update newArea.cs

--- newArea.cs
+++ newArea.cs
@@ -8,12 +8,56 @@ namespace Jake
 {
     class Move
     {
-        public Areas Location;
-        Areas Area0(Areas modLoc)
+        /*
+        public Areas Intro()
         {
-            modLoc.areaName = "area0";
-            modLoc.areaImage = "./images/area1.png";
-            modLoc.areaText = "You have entered area0.";
+            modLoc.areaName = "Intro";
+            modLoc.areaImage = "Intro";
+            modLoc.areaText = "Welcome.";
+            // modLoc.areaNorth = "You've hit a wall.  Good job.";
+            // modLocareaSouth = "There's no way.";
+            //            string areaWest = "There is no exit this way";
+            //            string areaEast = "Just a wall.";
+            modLoc.areaUp = "area0";
+            modLoc.areaDown = "Nothing down there.";
+            //            string[] areaItems;
+            //            areaItems = new string[];
+            return modLoc;
+        }
+        */
+        public static void area0()
+        {
+            Form1.Loc.areaName = "area0";
+            Form1.Loc.areaText = "You have entered area 0.";
+            //            string areaNorth = "You've hit a wall.  Good job.";
+            //            string areaSouth = "There's no way.";
+            //            string areaWest = "There is no exit this way";
+            //            string areaEast = "Just a wall.";
+            Form1.Loc.areaUp = "area1";
+            Form1.Loc.areaDown = "Just the floor.";
+            //            string[] areaItems;
+            //            areaItems = new string[];
+        }
+
+        public static void area1()
+        {
+            Form1.Loc.areaName = "area1";
+            Form1.Loc.areaText = "You have entered area 1.";
+            //            string areaNorth = "You've hit a wall.  Good job.";
+            //            string areaSouth = "There's no way.";
+            //            string areaWest = "There is no exit this way";
+            //            string areaEast = "Just a wall.";
+            Form1.Loc.areaUp = "You reach to the sky.";
+            Form1.Loc.areaDown = "area0";
+            //            string[] areaItems;
+            //            areaItems = new string[];
+        }
+        /*
+        public static Areas area2(Areas modLoc)
+        {
+            modLoc.areaName = "area2";
+            modLoc.areaImage = "./images/area2.png";
+            modLoc.areaText = "You have entered area 2.";
             //            string areaNorth = "You've hit a wall.  Good job.";
             //            string areaSouth = "There's no way.";
             //            string areaWest = "There is no exit this way";
@@ -21,9 +65,23 @@ namespace Jake
             //            string areaUp = "GoTo#Area1";
             //            string[] areaItems;
             //            areaItems = new string[];
-            Location = modLoc;
             return modLoc;
         }
+
+        public static Areas area3(Areas modLoc)
+        {
+            modLoc.areaName = "area3";
+            modLoc.areaImage = "./images/area3.png";
+            modLoc.areaText = "You have entered area 3.";
+            //            string areaNorth = "You've hit a wall.  Good job.";
+            //            string areaSouth = "There's no way.";
+            //            string areaWest = "There is no exit this way";
+            //            string areaEast = "Just a wall.";
+            //            string areaUp = "GoTo#Area1";
+            //            string[] areaItems;
+            //            areaItems = new string[];
+            return modLoc;
+        }
+         */
     }
 }
-
