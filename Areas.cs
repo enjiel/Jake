3d1c0347a474b57ba5e779a6e1b111ba63649d6e
Enjiel <enjielkaldwor@gmail.com>
2015-07-09 09:36:42 -0500

    Update Areas.cs

--- Areas.cs
+++ Areas.cs
@@ -9,29 +9,28 @@ namespace Jake
     public class Areas {
         public string areaName { get; set; }
         public string areaText { get; set; }
-//        public string areaDesc { get; set; }
-//        public string areaNorth { get; set; }
-//        public string areaSouth { get; set; }
-//        public string areaEast { get; set; }
-//        public string areaWest { get; set; }
+        public string areaDesc { get; set; }
+        public string areaNorth { get; set; }
+        public string areaSouth { get; set; }
+        public string areaEast { get; set; }
+        public string areaWest { get; set; }
         public string areaUp { get; set; }
         public string areaDown { get; set; }
 //        public string[] areaItems { get; set; }
 //        public string[] areaNPCs { get; set; }
 
-        public Areas(string areaName, string areaText /*, string areaDesc, string areaNorth, string areaSouth, string areaEast, string areaWest, string areaUp, string areaDown, string[] areaItems, string[] areaNPCs*/)
+        public Areas(string areaName, string areaText, string areaDesc, string areaNorth, string areaSouth, string areaEast, string areaWest, string areaUp, string areaDown/*, string[] areaItems, string[] areaNPCs*/)
         {
             this.areaName = areaName;
             this.areaText = areaText;
-//            areaNorth = areaNorth;
-//            areaSouth = areaSouth;
-//            areaEast = areaEast;
-//            areaWest = areaWest;
+            this.areaNorth = areaNorth;
+            this.areaSouth = areaSouth;
+            this.areaEast = areaEast;
+            this.areaWest = areaWest;
             this.areaUp = areaUp;
             this.areaDown = areaDown;
-//            areaItems = areaItems;
-//            areaNPCs = areaNPCs;
+//            this.areaItems = areaItems;
+//            this.areaNPCs = areaNPCs;
         }
     }
 }
-
